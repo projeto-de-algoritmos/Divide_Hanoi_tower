25e634e7242372f721fa17bdb0dc251670704187
Vitor Leal <vitor_leal2201@hotmail.com>
2020-11-10 22:56:31 -0300

    Init Discs and discs colors
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import React from 'react';
 import './style.css';
 
 const TOWERS_NUMBER = 3;
+const discColours = [
+  'gray',
+  'red',
+  'green',
+  'cyan',
+  'yellow',
+  'orange',
+  'magenta',
+  'blue',
+];
+
+const Disc = ({ size, topDisc, startDrag }) => {
+  const discWidth = (size + 1.5) * 25;
+  const discStyle = {
+    width: discWidth + 'px',
+    backgroundColor: discColours[size % 8],
+  };
+
+  return (
+    <div
+      className='disc'
+      style={discStyle}
+    >
+      <span className='disc-label'>
+        {size}
+      </span>
+    </div>
+  );
+}
 
 class Towers extends React.Component {
   constructor(props) {
