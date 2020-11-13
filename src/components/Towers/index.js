b29d08dc6cdb05e06411c593310e175c35d8abe0
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-11-13 01:09:20 -0300

    Change architecture
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

---
+++ src/components/Towers/index.js
@@ -0,0 +1,56 @@
+import _ from 'lodash';
+import React from 'react';
+import './style.css';
+import Tower from  '../Tower';
+
+const TOWERS_NUMBER = 3;
+
+class Towers extends React.Component {
+  constructor(props) {
+    super(props);
+    let startTower = _.range(1, this.props.discsNumber + 1);
+    let discs = _.map(Array(TOWERS_NUMBER), (val, i) =>
+      i === 0 ? startTower : []
+    );
+    this.state = { discs };
+  }
+
+  startTopDiscDrag(activeTower) {
+    this.activeTower = activeTower;
+  }
+
+  dropDisc(destTower) {
+    const sourceTower = this.activeTower;
+    this.activeTower = null;
+    if (sourceTower === destTower || sourceTower === null) return;
+
+    this.setState((state) => {
+      if(!state.discs[sourceTower]) return state;
+      const disc = state.discs[sourceTower][0];
+      if (state.discs[destTower][0] < disc) return state;
+
+      let discs = [...state.discs];
+      discs[sourceTower] = _.tail(discs[sourceTower]);
+      discs[destTower] = [disc, ...state.discs[destTower]];
+      return { discs };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.discs.map((towerDiscs, i) =>
+          <Tower
+            key={i + 1}
+            towerDiscs={towerDiscs}
+            maxSize={this.props.discsNumber}
+            startTopDiscDrag={() => this.startTopDiscDrag(i)}
+            dropDisc={() => this.dropDisc(i)}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Towers;
